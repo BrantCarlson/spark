c353b73c2772a8aeac2b054102b8743ceaf19237
zscherrer <zscherrer@carthage.edu>
2013-06-19 14:19:58 -0500

    Added rudimentary spike finder to DataPull

--- DataPull.py
+++ DataPull.py
@@ -7,16 +7,17 @@ Created on Wed Jun 12 11:09:29 2013
 
 import matplotlib.pyplot as plt
 import numpy as np
-import conf
 
-day = int(raw_input("Testing Day?"))
-scopeNo = int(raw_input("Oscilloscope Number?"))
-chan = int(raw_input("Channel Number?"))
-shot = int(raw_input("Shot Number?"))
+day = 22
+scopeNo = 3
+chan = 2
+shot = 0
 
-mydata = open(conf.dataDir+"/%d_01_2013_osc%d/C%dosc%d-%05d.txt" % (day, scopeNo, chan, scopeNo, shot), "r")
+mydata = open("C:/Users/Zach.Zach-PC/Documents/Carthage/Summer 2013/Flashdrive contents/sparkData_2013/sparkData/%d_01_2013_osc%d/C%dosc%d-%05d.txt" % (day, scopeNo, chan, scopeNo, shot), "r")
 x = mydata.readlines()
 data = x[5:]
+spikex = []
+spikey = []
 
 def splitit(data):
     i = 0
@@ -25,12 +26,19 @@ def splitit(data):
     while i < len(data):
         singlepoint = data[i].split(',')
         xplot == xplot.append(float(singlepoint[0]))
-        yplot == yplot.append(float(singlepoint[1]))
+        if float(singlepoint[1]) < -0.008:
+            spikey == spikey.append(0.05)
+            spikex == spikex.append(float(singlepoint[0]))
+            yplot == yplot.append(float(singlepoint[1]))
+        else:
+            spikey == spikey.append(0.0)
+            spikex == spikex.append(float(singlepoint[0]))
+            yplot == yplot.append(float(singlepoint[1]))
         i += 1
     return (xplot,yplot)
 (x,y) = splitit(data)
 
-j = np.array((x,y))
+j = np.array((spikex,spikey))
 print j
 
 line = plt.plot(x,y,'b-')
@@ -38,4 +46,8 @@ plt.ylabel("Amplitude")
 plt.xlabel("Time")
 plt.title("Scope " + str(scopeNo) +", Channel " +str(chan) + ", Shot " + str(shot) + " on Jan " + str(day))
 
+spike_finder = plt.plot(spikex,spikey,'r-')
+plt.ylabel("Amplitude")
+plt.xlabel("Time")
+plt.title("Scope " + str(scopeNo) +", Channel " +str(chan) + ", Shot " + str(shot) + " on Jan " + str(day))
 mydata.close()

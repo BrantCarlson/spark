56c0f7bd358d202c2b9825b4925bf77cd7aa2e75
Kevin LeCaptain <klecaptain@carthage.edu>
2013-06-20 13:21:38 -0500

    Add plot of original data

--- spike catalog test.py
+++ spike catalog test.py
@@ -13,7 +13,8 @@ scopeNo = 3
 chan = 2
 shot = 0
 
-mydata = open("C:/Users/Zach.Zach-PC/Documents/Carthage/Summer 2013/Flashdrive contents/sparkData_2013/sparkData/%d_01_2013_osc%d/C%dosc%d-%05d.txt" % (day, scopeNo, chan, scopeNo, shot), "r")
+mydata = open("C:/Sparks/lex/2013JanVisit/sparkData/%d_01_2013_osc%d/C%dosc%d-%05d.txt" % (day, scopeNo, chan, scopeNo, shot), "r")
+
 x = mydata.readlines()
 data = x[5:]
 spikex = []
@@ -21,16 +22,21 @@ spikey = []
 
 def splitit(data):
     i = 0
+    xplot = []
+    yplot = []
     while i < len(data):
         singlepoint = data[i].split(',')
+        xplot == xplot.append(float(singlepoint[0]))
         if float(singlepoint[1]) < -0.008:
             spikey == spikey.append(0.05)
             spikex == spikex.append(float(singlepoint[0]))
+            yplot == yplot.append(float(singlepoint[1]))
         else:
             spikey == spikey.append(0.0)
             spikex == spikex.append(float(singlepoint[0]))
+            yplot == yplot.append(float(singlepoint[1]))
         i += 1
-    return (spikex,spikey)
+    return (xplot,yplot)
 (x,y) = splitit(data)
 
 s = np.array((spikex,spikey))
@@ -51,8 +57,15 @@ def time_intervals(x):
     
 print time_intervals(times)
 
+line = plt.plot(x,y,'b-')
+plt.ylabel("Amplitude")
+plt.xlabel("Time")
+plt.title("Scope " + str(scopeNo) +", Channel " +str(chan) + ", Shot " + str(shot) + " on Jan " + str(day))
+
 spike_finder = plt.plot(spikex,spikey,'r-')
 plt.ylabel("Amplitude")
 plt.xlabel("Time")
 plt.title("Scope " + str(scopeNo) +", Channel " +str(chan) + ", Shot " + str(shot) + " on Jan " + str(day))
+plt.show()
+
 mydata.close()

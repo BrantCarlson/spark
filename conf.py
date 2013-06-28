618908deef5acf01e4e22e3d613e663822e5ec5d
Kevin LeCaptain <klecaptain@carthage.edu>
2013-06-28 13:06:42 -0500

    Kevin's conf.py

--- conf.py
+++ conf.py
@@ -2,5 +2,4 @@
 Configuration local to the user's computer goes here, to avoid constant conflicts in other files.
 """
 
-#for Brant's computer:
-dataDir = "/home/brant/lex/2013JanVisit/sparkData"
+dataDir = "C:/Sparks/lex/2013JanVisit/sparkData"  #for Kevin's computer

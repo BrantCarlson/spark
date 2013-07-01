ac231c12ef9904e40fdd4b750e490fc0269bb553
zscherrer <zscherrer@carthage.edu>
2013-07-01 15:46:13 -0500

    Still attempting to make Github like me

--- conf.py
+++ conf.py
@@ -3,8 +3,5 @@ Configuration local to the user's computer goes here, to avoid constant conflict
 """
 
 #for Brant's computer:
-<<<<<<< HEAD
-dataDir = "C:/Users/Zach.Zach-PC/Documents/Carthage/Summer 2013/Flashdrive contents/sparkData_2013/sparkData/"
-=======
 dataDir = "/home/brant/lex/2013JanVisit/sparkData"
->>>>>>> f21ad6acc210c094f5eb79bc6987858ae006e2eb
+

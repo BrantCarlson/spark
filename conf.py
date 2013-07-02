96130e470dfa51c3cecb1d02d5d10ba23e68b8f5
kweber <kweber3@carthage.edu>
2013-07-02 11:35:04 -0500

    New attempts at finding multiple spikes
    This is the first attempts at zeroing out previously found spikes.  It's
    in a continuous loop.

---
+++ conf.py
@@ -0,0 +1,6 @@
+"""
+Configuration local to the user's computer goes here, to avoid constant conflicts in other files.
+"""
+
+#for Brant's computer:
+dataDir = "C:/Users/Kyle Weber/Documents/Carthage/SURE/sparkData_2013/lex/2013JanVisit/sparkData"

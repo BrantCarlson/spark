ac8e27c00a13c8c252133e4985721cd38561138b
zscherrer <zscherrer@carthage.edu>
2013-07-03 15:32:42 -0500

    Fixed error mentioned in previous commit
    There was an error in Spike_Cataloger that only allowed for one spike
    per scope/channel combination in the data frame.  This error has been
    fixed

--- Spike_Cataloger.py
+++ Spike_Cataloger.py
@@ -70,29 +70,29 @@ for scopeNo in range(2,4):
                         end = x[index_count-1]
                         e_index = index_count
                         break
-            duration = end - start
-            peak = y.Ampl[s_index:e_index].min()
-            integral_a = si.trapz(y[s_index:e_index])
-            integral_b = np.sum(integral_a)
-            if duration > 3.0e-9 and start != 0:
-                #Checks the duration of the spike to throw out any false positives
-                #that would come from noise.  If the spike is not a false positive
-                #the data is appended to results and printed
-                results.append(start)
-                results.append(end)
-                results.append(peak)
-                results.append(duration)
-                results.append(day)
-                results.append(shot)
-                results.append(scopeNo)
-                results.append(chan)
-                results.append(integral_b)
-                print "Spike Duration: " + str(duration) + " seconds."
-                print "Peak: " + str(peak)
-                print "Start: " + str(start) + " seconds.", "End: " + str(end) + " seconds."
-            #Resets start and end so as to not report the last spike twice
-            start = 0
-            end = 0
+                duration = end - start
+                peak = y.Ampl[s_index:e_index].min()
+                integral_a = si.trapz(y[s_index:e_index])
+                integral_b = np.sum(integral_a)
+                if duration > 3.0e-9 and start != 0:
+                    #Checks the duration of the spike to throw out any false positives
+                    #that would come from noise.  If the spike is not a false positive
+                    #the data is appended to results and printed
+                    results.append(start)
+                    results.append(end)
+                    results.append(peak)
+                    results.append(duration)
+                    results.append(day)
+                    results.append(shot)
+                    results.append(scopeNo)
+                    results.append(chan)
+                    results.append(integral_b)
+                    print "Spike Duration: " + str(duration) + " seconds."
+                    print "Peak: " + str(peak)
+                    print "Start: " + str(start) + " seconds.", "End: " + str(end) + " seconds."
+                    #Resets start and end so as to not report the last spike twice
+                start = 0
+                end = 0
             return results
         spikex, spikey = threshold(y)
         results = time_intervals(spikex,spikey)

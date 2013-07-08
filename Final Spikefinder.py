f56e7eb5769ca758eaf088d0759e1113596a3a4c
kweber <kweber3@carthage.edu>
2013-07-08 10:48:07 -0500

    My work from Wednesday is gone.
    I hate my life.  Now I'm redoing it.

---
+++ Final Spikefinder.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 08 10:46:17 2013
+
+@author: Kyle Weber
+"""
+
--- conf.py
+++ conf.py
@@ -2,5 +2,5 @@
 Configuration local to the user's computer goes here, to avoid constant conflicts in other files.
 """
 
-#for Brant's computer:
-dataDir = "C:/Users/Kyle Weber/Documents/Carthage/SURE/sparkData_2013/lex/2013JanVisit/sparkData"
+#for Kyle's computer:
+kdataDir = "C:/Users/Kyle Weber/Documents/Carthage/SURE/sparkData_2013/lex/2013JanVisit/sparkData"

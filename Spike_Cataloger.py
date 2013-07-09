13c196c92d0f7f5b72633061372b8ca9d7089a6d
zscherrer <zscherrer@carthage.edu>
2013-07-09 16:19:55 -0500

    Actually fixed the error in time_intervals function
    Turns out that the last commit didn't fix the error where time_intervals
    would only return one spike per day/shot/scope/channel combination, but
    it has been fixed now.  However, now Spike_Cataloger returns far too
    many spikes total (120,324 total).  I think it would be worth making the
    threshold less sensitive.

--- Spike_Cataloger.py
+++ Spike_Cataloger.py
@@ -17,67 +17,69 @@ import toolbox
 #chan = 2
 #shot = 0
 
-        
+
 def time_intervals(x,z):
-                #takes two lists as arguments
-                #Defining variables
-                results = []
-                start = 0.0
-                end = 0.0
-                duration = 0.0
-                index_count = 0
-                s_index = 0
-                e_index = 0
-                peak = 0.0
-                #Loops through indices and looks for beginnings and ends of spikes
-                while index_count < len(z) - 2:
-                    for i in z[index_count:]:
-                        index_count += 1
-                        if i == 0.05 and z[index_count-1] == 0.05:
-                            start = x[index_count-1]
-                            s_index = index_count
-                            break
-                    for i in z[index_count:]:
-                        index_count += 1
-                        if i == 0.0 and z[index_count-1] == 0.05:
-                            end = x[index_count-1]
-                            e_index = index_count
-                            break
-                        elif index_count >= len(z) - 3:
-                            #Breaks if no end has been found for a spike by the end of z
-                            end = x[index_count-1]
-                            e_index = index_count
-                            break
-                    duration = end - start
-                    peak = y.Ampl[s_index:e_index].min()
-                    integral_a = si.trapz(y[s_index:e_index])
-                    integral_b = np.sum(integral_a)
-                    if duration > 3.0e-9 and start != 0:
-                        #Checks the duration of the spike to throw out any false positives
-                        #that would come from noise.  If the spike is not a false positive
-                        #the data is appended to results and printed
-                        results.append(start)
-                        results.append(end)
-                        results.append(peak)
-                        results.append(duration)
-                        results.append(day)
-                        results.append(shot)
-                        results.append(scopeNo)
-                        results.append(chan)
-                        results.append(integral_b)
-                        print "Spike Duration: " + str(duration) + " seconds."
-                        print "Peak: " + str(peak)
-                        print "Start: " + str(start) + " seconds.", "End: " + str(end) + " seconds."
-                        #Resets start and end so as to not report the last spike twice
-                    start = 0
-                    end = 0
-                return results
+    #takes two lists as arguments
+    #Defining variables
+    results = []
+    start = 0.0
+    end = 0.0
+    duration = 0.0
+    index_count = 0
+    s_index = 0
+    e_index = 0
+    peak = 0.0
+    #Loops through indices and looks for beginnings and ends of spikes
+    while index_count < len(z) - 2:
+        for i in z[index_count:]:
+            index_count += 1
+            if i == 0.05 and z[index_count-1] == 0.05:
+                start = x[index_count-1]
+                s_index = index_count
+                break
+        for i in z[index_count:]:
+            index_count += 1
+            if z[index_count-1] == 0.00 and z[index_count-2] == 0.05:
+                end = x[index_count-1]
+                e_index = index_count
+                break
+            elif index_count >= len(z) - 5:
+                #Breaks if no end has been found for a spike by the end of z
+                end = x[index_count-1]
+                e_index = index_count
+                break
+        duration = end - start
+        peak = y.Ampl[s_index:e_index].min()
+        integral_a = si.trapz(y[s_index:e_index])
+        integral_b = np.sum(integral_a)
+        if duration > 3.0e-9 and start != 0:
+            #Checks the duration of the spike to throw out any false positives
+            #that would come from noise.  If the spike is not a false positive
+            #the data is appended to results and printed
+            results.append(start)
+            results.append(end)
+            results.append(peak)
+            results.append(duration)
+            results.append(day)
+            results.append(shot)
+            results.append(scopeNo)
+            results.append(chan)
+            results.append(integral_b)
+            print "Spike Duration: " + str(duration) + " seconds."
+            print "Peak: " + str(peak)
+            print "Start: " + str(start) + " seconds.", "End: " + str(end) + " seconds."
+            print str(day) , str(shot) , str(scopeNo) , str(chan)
+            #Resets start and end so as to not report the last spike twice
+            start = 0
+            end = 0
+    return results
 
 spikex = []
 spikey = []
 final_results = []
 n_smooth = 25
 significance = 12
+
 
 for day in range(22,27):
     if day == 22:
@@ -91,6 +93,7 @@ for day in range(22,27):
                     spikex, spikey = y.Time, toolbox.threshold(y)
                     results = time_intervals(spikex,spikey)
                     final_results.append(results)
+                    
     elif day == 23:
         for shot in range(0,200):
             for scopeNo in range(2,4):
@@ -136,9 +139,11 @@ for day in range(22,27):
                     results = time_intervals(spikex,spikey)
                     final_results.append(results)
     
+
 spike_info = toolbox.list_to_frame(final_results)
 
 print spike_info
+
 """
 plt.plot(y.Time,y.Ampl)
 plt.ylabel("Amplitude")
--- toolbox.py
+++ toolbox.py
@@ -58,59 +58,60 @@ def threshold(y):
 
 #Zack's version of time_intervals
 def time_intervals(x,z):
-                #takes two lists as arguments
-                #Defining variables
-                results = []
-                start = 0.0
-                end = 0.0
-                duration = 0.0
-                index_count = 0
-                s_index = 0
-                e_index = 0
-                peak = 0.0
-                #Loops through indices and looks for beginnings and ends of spikes
-                while index_count < len(z) - 2:
-                    for i in z[index_count:]:
-                        index_count += 1
-                        if i == 0.05 and z[index_count-1] == 0.05:
-                            start = x[index_count-1]
-                            s_index = index_count
-                            break
-                    for i in z[index_count:]:
-                        index_count += 1
-                        if i == 0.0 and z[index_count-1] == 0.05:
-                            end = x[index_count-1]
-                            e_index = index_count
-                            break
-                        elif index_count >= len(z) - 3:
-                            #Breaks if no end has been found for a spike by the end of z
-                            end = x[index_count-1]
-                            e_index = index_count
-                            break
-                    duration = end - start
-                    peak = y.Ampl[s_index:e_index].min()
-                    integral_a = si.trapz(y[s_index:e_index])
-                    integral_b = np.sum(integral_a)
-                    if duration > 3.0e-9 and start != 0:
-                        #Checks the duration of the spike to throw out any false positives
-                        #that would come from noise.  If the spike is not a false positive
-                        #the data is appended to results and printed
-                        results.append(start)
-                        results.append(end)
-                        results.append(peak)
-                        results.append(duration)
-                        results.append(day)
-                        results.append(shot)
-                        results.append(scopeNo)
-                        results.append(chan)
-                        results.append(integral_b)
-                        print "Spike Duration: " + str(duration) + " seconds."
-                        print "Peak: " + str(peak)
-                        print "Start: " + str(start) + " seconds.", "End: " + str(end) + " seconds."
-                        #Resets start and end so as to not report the last spike twice
-                    start = 0
-                    end = 0
-                return results
+    #takes two lists as arguments
+    #Defining variables
+    results = []
+    start = 0.0
+    end = 0.0
+    duration = 0.0
+    index_count = 0
+    s_index = 0
+    e_index = 0
+    peak = 0.0
+    #Loops through indices and looks for beginnings and ends of spikes
+    while index_count < len(z) - 2:
+        for i in z[index_count:]:
+            index_count += 1
+            if i == 0.05 and z[index_count-1] == 0.05:
+                start = x[index_count-1]
+                s_index = index_count
+                break
+        for i in z[index_count:]:
+            index_count += 1
+            if z[index_count-1] == 0.00 and z[index_count-2] == 0.05:
+                end = x[index_count-1]
+                e_index = index_count
+                break
+            elif index_count >= len(z) - 5:
+                #Breaks if no end has been found for a spike by the end of z
+                end = x[index_count-1]
+                e_index = index_count
+                break
+        duration = end - start
+        peak = y.Ampl[s_index:e_index].min()
+        integral_a = si.trapz(y[s_index:e_index])
+        integral_b = np.sum(integral_a)
+        if duration > 3.0e-9 and start != 0:
+            #Checks the duration of the spike to throw out any false positives
+            #that would come from noise.  If the spike is not a false positive
+            #the data is appended to results and printed
+            results.append(start)
+            results.append(end)
+            results.append(peak)
+            results.append(duration)
+            results.append(day)
+            results.append(shot)
+            results.append(scopeNo)
+            results.append(chan)
+            results.append(integral_b)
+            print "Spike Duration: " + str(duration) + " seconds."
+            print "Peak: " + str(peak)
+            print "Start: " + str(start) + " seconds.", "End: " + str(end) + " seconds."
+            print str(day) , str(shot) , str(scopeNo) , str(chan)
+            #Resets start and end so as to not report the last spike twice
+            start = 0
+            end = 0
+    return results
 
 
 def list_to_frame(r):

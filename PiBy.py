f9889880ad5e41ed7dc1cfa9589129f41ddcdc00
kweber <kweber3@carthage.edu>
2013-07-15 11:06:35 -0500

    This is Kyle's binary input
    I'm trying to make a function to simplify comparing spike stuff.

---
+++ PiBy.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 12 10:04:23 2013
+
+@author: Kyle Weber
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+z = pd.load('C:/Users/Kyle Weber/Documents/GitHub/spark/SparkSpikeStuff')
+print z

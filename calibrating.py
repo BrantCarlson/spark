09b2fb96867e0baaf2f7997f8ccd51f88d04c922
zscherrer <zscherrer@carthage.edu>
2013-07-23 15:32:13 -0500

    Added comments to calibrating.py

--- calibrating.py
+++ calibrating.py
@@ -14,36 +14,36 @@ a = pd.load("C:/Program Files (x86)/pythonxy/a_programs/spike_info")
 b = pd.load("C:/Program Files (x86)/pythonxy/a_programs/spike_info_sig200")
 z = pd.load('C:/Users/Zach.Zach-PC/Documents/GitHub/spark/SparkSpikeStuff')
 
-a.shot = np.array(map(int, a.Shot))
+d = a.set_index(['Day', 'Scope', 'Channel', 'Shot']) #heirachrically indexing dataframe a
 
-d = a.set_index(['Day', 'Scope', 'Channel', 'Shot'])
+#Creating a dataframe of the useful data from the radial shots
+ai = d.Peak.ix[23].ix[3].groupby(level =['Shot', 'Channel']).min() #a dataframe of the biggest peak in each shot/channel combination on day 23, scope 3
+aiu = ai.unstack(1) #Channels are now the columns, shot number is the index
+aiu = aiu.fillna(0) #Replacing all NaNs with 0
 
-#sel = np.logical_and(a.Day == 24, a.Scope == 3)
-#sc = a[a.Day == 24][a.Scope == 3]
-ai = d.Peak.ix[23].ix[3].groupby(level =['Shot', 'Channel']).min()
-aiu = ai.unstack(1)
-aiu = aiu.fillna(0)
-
+#Creating a dataframe of the calibration data.  Repeating same processes done before.
 ci = d.Peak.ix[25].ix[3].groupby(level =['Shot', 'Channel']).min()
 ciu = ci.unstack(1)
 ciu = ciu.fillna(0)
 
-ciu = ciu[ciu[1] > -0.46][ciu[2] > -0.44][ciu[3] > -0.43][ciu[4] > -0.043]
+ciu = ciu[ciu[1] > -0.46][ciu[2] > -0.44][ciu[3] > -0.43][ciu[4] > -0.043] #Removing saturated data from each channel
 
-count = 0
-for e in range(1,5):
+#Two loops to plot channels against each other
+count = 0  
+for e in range(1,5): 
     for f in range(1,5):
-        if f > e:
+        if f > e: #f and e both act as variables for a channel.  This line makes sure that there are no redundant plots.
             count += 1
-            fig = plt.figure(count)
+            fig = plt.figure(count) #Creates a separate figure for each plot
             #To plot data from radial shots instead of calibration shots,
             #change ciu to aiu and change 0:50 to 0:100 in the plt.scatter function
-            plt.scatter(-ciu[f][0:50], -ciu[e][0:50])
+            plt.scatter(-ciu[f][0:50], -ciu[e][0:50]) #Creates a scatter plot of the two channels
             plt.xlabel('Channel ' + str(f) + ' Amplitude')
             plt.ylabel('Channel ' + str(e) + ' Amplitude')
             
-            slope, intercept, r_value, p_value, error= stats.linregress(-ciu[f][0:50],-ciu[e][0:50])
-            plt.plot([0, 0.5], [intercept, slope / 2], "r-")
+            slope, intercept, r_value, p_value, error= stats.linregress(-ciu[f][0:50],-ciu[e][0:50]) #Gives calibration statistics for each plot
+            plt.plot([0, 0.5], [intercept, slope / 2], "r-") #Plots a calibration line over the scatter plot
+            #Reporting statistics for the plot            
             print "---"
             print "X-axis: Channel " + str(f) + "  Y-axis: Channel " + str(e) 
             print "Slope: " + str(slope) + "  Intercept: " + str(intercept)
